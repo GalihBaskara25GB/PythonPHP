7952be3cc82a7e3cbcd7edc64f29c9e0d7fa645f
Galih <galihbaskara25.gb@gmail.com>
2020-06-12 19:25:56 +0700

    Add Chart Selector
    Adding a new dropdown selector for choose chart , chart shown in the same page.

--- execPython.php
+++ execPython.php
@@ -6,7 +6,7 @@
     <body>
         <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3 bg-dark text-white" style="min-height: 668px">
+            <div class="col-md-2 bg-dark text-white" style="min-height: 668px">
                 <center>
                     <svg class="bi bi-person-circle mt-4 mb-2" width="80" height="80" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
@@ -17,26 +17,59 @@
                     <p>Administrator</p>
                 </center>
             </div>
-            <div class="col-md-9 pt-2">
-
+            <div class="col-md-10 pt-2">
+            
+            <div class="col-md-12 row">
+            
+            <?php
+                if(isset($_GET['chart'])){
+                    
+                    $chart = $_GET['chart'];
+                    if($chart=='timeseries') {
+                        $b = 'active'; $a = '';
+                    } else {
+                        $a = 'active';
+                        $b = '';
+                    }
+                
+                } else{
+                    $chart = 'kmeans';
+                    $a = 'active'; $b = '';
+                }
+            ?>
+            <div class="col-md-2">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Choose Chart
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="?chart=kmeans">K-Means</a>
+                        <a class="dropdown-item" href="?chart=timeseries">Time Series</a>
+                    </div>
+                </div>
+            </div>
+                    
     <?php
-        $file = 'D:/xampp/htdocs/PythonPHP/VisualizingData.py';
+        $file = 'D:/xampp/htdocs/PythonPHP/'.$chart.'.py';
         if(file_exists($file)) {
-            echo '<div class="alert alert-info" role="alert">File is exist, and placed in '.$file.'</div>';
+            echo '<div class="alert alert-info col-md-10" role="alert">File is exist, and placed in '.$file.'</div></div>';
             $command = escapeshellcmd($file);
             $output = shell_exec($command);
+
             if($output) {
                 echo '
                 <div class="embed-responsive embed-responsive-16by9" style="border: 1px solid grey; border-radius: 5px;">
                     <iframe id="igraph" class="embed-responsive-item" style="border:none;" scrolling="no" seamless="seamless" 
-                src="report.html" height="525" width="100%">
+                src="'.$chart.'.html" height="525" width="100%">
                     </iframe>
                 </div>';
             }
+
             else echo '<div class="alert alert-warning" role="alert">
                         Failed: Unable to generate report :(
                         </div>';
         }
+
         else echo '<div class="alert alert-warning" role="alert">
                         Warning: The path is incorrect or file is not exist!
                     </div>';
@@ -46,9 +79,9 @@
         </div>
 
     <!-- javasvript dependencies -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>    
-
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
     </body>
 </html>
--- timeseries.py
+++ timeseries.py
@@ -6,4 +6,9 @@ import pandas as pd
 df = pd.read_excel('C:/Users/ASUS/Downloads/DataUdara.xlsx')
 
 fig = px.line(df, x='Date', y='RH', hover_data=['Time', 'CO(GT)', 'PT08.S1(CO)', 'NMHC(GT)', 'C6H6(GT)', 'PT08.S2(NMHC)', 'NOx(GT)', 'PT08.S3(NOx)', 'NO2(GT)', 'PT08.S4(NO2)', 'PT08.S5(O3)', 'T', 'AH'])
-pio.write_html(fig, file='timeseries.html', auto_open=True)
+
+try:
+    pio.write_html(fig, file='timeseries.html', auto_open=False)
+    print(True)
+except:
+    print(False)
