5e3947ff42ea7faf4951c1e9a41236e04af3665e
Galih <galihbaskara25.gb@gmail.com>
2020-06-15 15:36:19 +0700

    Clustering Air Quality Data
    Change kmeans target, from AirBnB Dataset to Air Quality Dataset

--- kmeans.py
+++ kmeans.py
@@ -1,45 +1,91 @@
 import pandas as pd
+import numpy as np
+
 import plotly.express as px
+import plotly.graph_objects as pgo
+import chart_studio.plotly as py
 import plotly.io as pio
-df = pd.read_csv('http://localhost/PythonPHP/airbnb.csv') # path to  csv
 
-def clean_data(df):
-    
-    # replace numbers with strings
-    df.neighborhood = df.neighborhood.map({1 : 'Friedrichshain-Kreuzberg', 2 : 'Mitte', 3 : 'Pankow', 4 : 'Neukölln', 5 :'Charlottenburg-Wilm',
-                        6 : 'Tempelhof - Schöneberg', 7 : 'Lichtenberg', 8 : 'Treptow - Köpenick', 9 : 'Steglitz - Zehlendorf',
-                        10 : 'Reinickendorf', 11 : 'Marzahn - Hellersdorf', 12 : 'Spandau'})
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
 
-    df.room_type = df.room_type.map({1 : 'Entire home/apt', 2 : 'Private room', 3 : 'Shared room'})
+air = pd.read_excel('http://localhost/PythonPHP/DataUdara.xlsx') # path to  dataset
+air.head()
 
-    yes_no_dict = {0: 'No', 1:'Yes'}
-    df.wifi = df.wifi.map(yes_no_dict)
-    df.washer = df.washer.map(yes_no_dict)
-    df.cable_tv = df.cable_tv.map(yes_no_dict)
-    df.kitchen = df.kitchen.map(yes_no_dict)
-    
-    # rename columns
-    df.rename(columns={'neighborhood': 'Neighborhood', 'room_type': 'Room Type', 'accommodates': 'Accommodates',
-                      'bedrooms': 'Bedrooms', 'number_of_reviews': 'Number of Reviews', 'wifi': 'Wifi', 'cable_tv': 'Cable TV',
-                      'washer': 'Washer', 'kitchen': 'Kitchen', 'price': 'Price (US Dollars)'}, inplace=True)
-    
-    # remove outliers
-    df = df[df['Price (US Dollars)']<501]
-    
-    return df
+air = air.rename(columns={'NMHC(GT)' : 'NMHC'})
+air = air.drop(["Date","Time","CO(GT)","PT08.S1(CO)","C6H6(GT)","PT08.S2(NMHC)","AH","T","PT08.S5(O3)","PT08.S4(NO2)","NO2(GT)","PT08.S3(NOx)","NOx(GT)"], axis = 1)
 
-# clean data
-df = clean_data(df)
+plt.scatter(air.NMHC , air.RH, s =10, c = "c")
 
-fig = px.scatter(df, x='Neighborhood', y='Price (US Dollars)'
-                 ,size='Accommodates'
-                 , hover_data=['Bedrooms', 'Wifi', 'Cable TV', 'Kitchen', 'Washer', 'Number of Reviews']
-                 ,color= 'Room Type')
-fig.update_layout(template='plotly_white')
-fig.update_layout(title='How much should you charge in a Berlin neighborhood?')
+air_x = air.iloc[:, 0:2]
+air_x.head()
+x_array = np.array(air_x)
+scaler = MinMaxScaler()
+x_scaled = scaler.fit_transform(x_array)
+n_cluster = 5
+
+kmeans = KMeans(n_clusters = n_cluster, random_state=123)
+kmeans.fit(x_scaled)
+
+air["kluster"] = kmeans.labels_
+
+##Plot the K-Means Result
+# output = plt.scatter(x_scaled[:,0], x_scaled[:,1], s = 100, c = air.kluster, marker = "o", alpha = 1, )
+centers = kmeans.cluster_centers_
+# plt.scatter(centers[:,0], centers[:,1], c='red', s=200, alpha=1 , marker="o")
+# plt.title("Hasil Klustering K-Means")
+# plt.colorbar (output)
+# plt.show()
+
+print("Entering new Area...")
+
+trace0 = pgo.Scatter(x=x_scaled[:,0],
+                     y=x_scaled[:,1],
+                     text=air.kluster,
+                     name='Member of Cluster',
+                     mode='markers',
+                     marker=pgo.scatter.Marker(
+                                       size=26,
+                                       opacity=0.8,
+                                       color='Orange',
+                                       line=dict(
+                                            color='DarkSlateGray',
+                                            width=2)
+                                        ),
+                     showlegend=True                     
+)
+
+trace1 = pgo.Scatter(x=centers[:, 0],
+                     y=centers[:, 1],
+                     name='Centroids',
+                     mode='markers',
+                     marker=pgo.scatter.Marker(symbol='x',
+                                       size=16,
+                                       color='MediumPurple'
+                                        ),
+                     showlegend=True
+)
+
+data7 = [trace0, trace1]
+
+layout7 = pgo.Layout(
+                     xaxis=pgo.layout.XAxis(showgrid=True,
+                                     zeroline=False,
+                                     showticklabels=True),
+                     yaxis=pgo.layout.YAxis(showgrid=True,
+                                     zeroline=False,
+                                     showticklabels=True),
+                     hovermode='closest')
+
+layout7['title'] = 'Air Quality K-Means Clustering filtered by NMHC and RH'
+print("Finished Rendering layout7")
+
+fig = pgo.Figure(data=data7, layout=layout7)
 
 try:
     pio.write_html(fig, file='kmeans.html', auto_open=False)
     print(True)
 except:
     print(False) 
+
